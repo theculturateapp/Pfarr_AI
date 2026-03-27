4ddf0d7482a344630f37ebee2a944fb3e016af37
theculturateapp <120157187+theculturateapp@users.noreply.github.com>
2026-03-26 23:08:15 -0400

    backticks

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "37674b4ao471bb32b44ta37b064ef2da";
     let prompt = `Generate a haiku based on ${instructionsInput.value}`;
-    let context = "You are a grade schooler who likes to write poems. Your mission is to generate a 4 line haiku in basic HTML. Make sure to follow user inputs";
+    let context = `You are a grade schooler who likes to write poems. Your mission is to generate a 4 line haiku in basic HTML. Make sure to follow user inputs`;
     let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}"
 
     console.log("Generating Poem");
