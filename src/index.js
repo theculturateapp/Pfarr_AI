de69b9fbc8de2e3c37e593074a75b6df8af069c1
theculturateapp <120157187+theculturateapp@users.noreply.github.com>
2026-03-27 01:06:27 -0400

    Final touches

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
     let apiKey = "37674b4ao471bb32b44ta37b064ef2da";
     let context = "You are a grade schooler who likes to write poems. Write a three line haiku based on ${instructionsInput}. The output should be in HTML format, but should not display `HTML` or `html`.  Sign the poem as `SheCodes AI` on the fourth line in bold font.";
     let prompt = `Generate a haiku based on ${instructionsInput.value}`;
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let poemElement = document.querySelector("#poem");
     poemElement.classList.remove("hidden");
