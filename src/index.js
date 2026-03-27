7d47c64e67d352bfeb6b9f361c0e9d5d66f31590
theculturateapp <120157187+theculturateapp@users.noreply.github.com>
2026-03-26 23:04:21 -0400

    Added APIs for poem generation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-    let poemElement = document.querySelector("#poem");
-
-    new Typewriter('#poem', {
-  strings: "La tombe dit a la rose",
+function displayPoem(response) {
+ 
+ console.log("poem generated");   
+ new Typewriter('#poem', {
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
+}
 
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "37674b4ao471bb32b44ta37b064ef2da";
+    let prompt = `Generate a haiku based on ${instructionsInput.value}`;
+    let context = "You are a grade schooler who likes to write poems. Your mission is to generate aikus in basic HTML. Make sure to follow user inputs";
+    let apiUrl = "https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}"
+
+    console.log("Generating Poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context: ${context}`);
+
+    axios.get(apiUrl).then(displayPoem);
 };
 
 let poemFormElement = document.querySelector("#poem-generator-form");
